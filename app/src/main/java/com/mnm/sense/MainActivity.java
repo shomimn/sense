cbc944458e23d7aa09a1b345d7d7eb399faceb45
shomimn <shomimn@gmail.com>
2016-09-22 20:20:27 +0200

    working wifi probe test

--- app/src/main/java/com/mnm/sense/MainActivity.java
+++ app/src/main/java/com/mnm/sense/MainActivity.java
@@ -1,14 +1,168 @@
 package com.mnm.sense;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Handler;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import edu.mit.media.funf.FunfManager;
+import edu.mit.media.funf.json.IJsonObject;
+import edu.mit.media.funf.pipeline.BasicPipeline;
+import edu.mit.media.funf.probe.Probe;
+import edu.mit.media.funf.probe.builtin.SimpleLocationProbe;
+import edu.mit.media.funf.probe.builtin.WifiProbe;
+import edu.mit.media.funf.storage.NameValueDatabaseHelper;
+
+public class MainActivity extends AppCompatActivity implements Probe.DataListener
 {
+    public static final String PIPELINE_NAME = "default";
+    private static final String TOTAL_COUNT_SQL = "SELECT count(*) FROM " + NameValueDatabaseHelper.DATA_TABLE.name;
+    private FunfManager funfManager;
+    private BasicPipeline pipeline;
+    private WifiProbe wifiProbe;
+    private SimpleLocationProbe locationProbe;
+    private CheckBox enabledCheckbox;
+    private Button archiveButton, scanNowButton;
+    private TextView dataCountView;
+    private Handler handler;
+    private ServiceConnection funfManagerConn = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            funfManager = ((FunfManager.LocalBinder)service).getManager();
+
+            Gson gson = funfManager.getGson();
+            wifiProbe = gson.fromJson(new JsonObject(), WifiProbe.class);
+            locationProbe = gson.fromJson(new JsonObject(), SimpleLocationProbe.class);
+            pipeline = (BasicPipeline) funfManager.getRegisteredPipeline(PIPELINE_NAME);
+            wifiProbe.registerPassiveListener(MainActivity.this);
+            locationProbe.registerPassiveListener(MainActivity.this);
+
+            // This checkbox enables or disables the pipeline
+            enabledCheckbox.setChecked(pipeline.isEnabled());
+            enabledCheckbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if (funfManager != null) {
+                        if (isChecked) {
+                            funfManager.enablePipeline(PIPELINE_NAME);
+                            pipeline = (BasicPipeline) funfManager.getRegisteredPipeline(PIPELINE_NAME);
+                        } else {
+                            funfManager.disablePipeline(PIPELINE_NAME);
+                        }
+                    }
+                }
+            });
+
+            // Set UI ready to use, by enabling buttons
+            enabledCheckbox.setEnabled(true);
+            archiveButton.setEnabled(true);
+            scanNowButton.setEnabled(true);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            funfManager = null;
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Displays the count of rows in the data
+        dataCountView = (TextView) findViewById(R.id.dataCountText);
+
+        // Used to make interface changes on main thread
+        handler = new Handler();
+
+        enabledCheckbox = (CheckBox) findViewById(R.id.enabledCheckbox);
+        enabledCheckbox.setEnabled(false);
+
+        // Runs an archive if pipeline is enabled
+        archiveButton = (Button) findViewById(R.id.archiveButton);
+        archiveButton.setEnabled(false);
+        archiveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (pipeline.isEnabled()) {
+                    pipeline.onRun(BasicPipeline.ACTION_ARCHIVE, null);
+
+                    // Wait 1 second for archive to finish, then refresh the UI
+                    // (Note: this is kind of a hack since archiving is seamless and there are no messages when it occurs)
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getBaseContext(), "Archived!", Toast.LENGTH_SHORT).show();
+                            updateScanCount();
+                        }
+                    }, 1000L);
+                } else {
+                    Toast.makeText(getBaseContext(), "Pipeline is not enabled.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        // Bind to the service, to create the connection with FunfManager
+        bindService(new Intent(this, FunfManager.class), funfManagerConn, BIND_AUTO_CREATE);
+
+        scanNowButton = (Button) findViewById(R.id.scanNowButton);
+        scanNowButton.setEnabled(false);
+        scanNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (pipeline.isEnabled()) {
+                    // Manually register the pipeline
+                    wifiProbe.registerListener(pipeline);
+                    locationProbe.registerListener(pipeline);
+                } else {
+                    Toast.makeText(getBaseContext(), "Pipeline is not enabled.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onDataReceived(IJsonObject probeConfig, IJsonObject data)
+    {
+
+    }
+
+    @Override
+    public void onDataCompleted(IJsonObject probeConfig, JsonElement checkpoint)
+    {
+        updateScanCount();
+        // Re-register to keep listening after probe completes.
+        wifiProbe.registerPassiveListener(this);
+        locationProbe.registerPassiveListener(this);
+    }
+
+    private void updateScanCount() {
+        // Query the pipeline db for the count of rows in the data table
+        SQLiteDatabase db = pipeline.getDb();
+        Cursor cursor = db.rawQuery("select * from " + NameValueDatabaseHelper.DATA_TABLE.name, null);
+        while (cursor.moveToNext())
+        {
+            String id = cursor.getString(0);
+            String device = cursor.getString(1);
+            long timestamp = cursor.getLong(2);
+            String value = cursor.getString(3);
+        }
     }
 }
